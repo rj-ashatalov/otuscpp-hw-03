1954b22e35fb5a8916f81baa08b1695542b3c5a6
Shatalov Andrey <a.shatalov@rj-games.com>
2019-10-24 00:57:16 +0300

    cleanup

--- tests/test_version.cpp
+++ tests/test_version.cpp
@@ -26,13 +26,5 @@ BOOST_AUTO_TEST_CASE(ip_filter_full_test)
 
     std::ifstream testStream{ "../ip_filter.tst"};
     BOOST_CHECK(output.str() == toString(fill(&testStream)));
-
-//    BOOST_CHECK(filter(ipPool, -1).size() <= 0);
-//    BOOST_CHECK(filter(ipPool, 78).size() >= 2);
-//    BOOST_CHECK(filter(ipPool, 78).size() >= 2);
-//    BOOST_CHECK(filter(ipPool, 5, 8).size() == 6);
-//    BOOST_CHECK(filter(ipPool, 8, 5).size() <= 0);
-//    BOOST_CHECK(filter(ipPool, 46, 70, 113).size() >= 1);
-//    BOOST_CHECK(filter(ipPool, 46, 70, 113, 73, 0, 1, 2, 3) != filter(ipPool, 46, 70, 113, 73));
 }
 BOOST_AUTO_TEST_SUITE_END()
