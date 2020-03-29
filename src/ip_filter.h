e83440ec7c2b12f732e913801ad7476f3910f05d
Andrey Shatalov <a.shatalov@rj-games.com>
2020-03-29 21:25:21 +0300

    output via ranges

--- src/ip_filter.h
+++ src/ip_filter.h
@@ -58,25 +58,15 @@ IpPool fill(std::istream* input)
 
 std::string toString(const IpPool& pool)
 {
-    auto res = pool | ranges::views::transform([&](const auto& ip)
+    auto ips = pool | ranges::views::transform([&](const auto& ip)
     {
         auto res = ip | ranges::views::transform([&](const auto& item)
         {
             return std::to_string(item);
         }) | ranges::views::intersperse(".");
         return ranges::actions::join(res) | ranges::to<std::string>();
-    });
-
-    std::stringstream output;
-    ranges::copy(res, ranges::ostream_iterator<>(output, "\n"));
-    return output.str();
-
-/*    for (const auto& ip : pool)
-    {
-        std::copy(ip.begin(), std::prev(ip.end()), std::ostream_iterator<int>(output, "."));
-        output << ip.back() << std::endl;
-    }
-    return output.str();*/
+    }) | ranges::views::intersperse("\n");
+    return ranges::actions::join(ips) | ranges::to<std::string>();
 }
 
 template<class T>
